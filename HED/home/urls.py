7c375b2f02c13b69e9979b1d0396d14d36154709
hamza48 <rguighamza0@gmail.com>
2021-01-03 18:55:17 +0100

    updating the urls and css file

--- HED/home/urls.py
+++ HED/home/urls.py
@@ -5,7 +5,8 @@ from django.conf.urls import url
 urlpatterns = [
         #Leave as empty string for base url
 	path('', views.home, name="home"),
-	path('register', views.register, name="register"),
+	path('loggin', views.logginUser, name="logginUser"),
 	path('logout',views.logout, name = 'logout'),
 	path('profile', views.profile, name = 'profile'),
+	path('register', views.register, name = 'register'),
 ]
