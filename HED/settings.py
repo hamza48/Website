4c8e619ed81bc8a78193bc3527e78c02e91b279c
hamza48 <rguighamza0@gmail.com>
2021-04-06 20:14:37 +0200

    modify setting.py

--- HED/settings.py
+++ HED/settings.py
@@ -12,9 +12,9 @@ https://docs.djangoproject.com/en/3.1/ref/settings/
 from pathlib import Path
 import os
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
-BASE_DIR = Path(__file__).resolve(strict=True).parent.parent
+#BASE_DIR = Path(__file__).resolve(strict=True).parent.parent
 
-
+BASE_DIR = (dirname(abspath(__file__)))
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
